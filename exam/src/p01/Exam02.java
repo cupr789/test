27a4e39296926606d5c54c9e948e50a94cadc8f0
Administrator <Administrator@DJA-PC>
2017-11-21 18:31:41 +0900

    project init

---
+++ exam/src/p01/Exam02.java
@@ -0,0 +1,40 @@
+package p01;
+
+import java.util.Scanner;
+
+public class Exam02 {
+	public static void main(String[] args) {
+		/*while (true) {
+			Scanner s = new Scanner(System.in);
+
+			String s1 = s.nextLine();
+			if (s1.equals("exit")) {
+				System.out.println("종료됩니다.");
+				break;
+			}
+			int a = s.nextInt();
+			int b = s.nextInt();
+
+			if (s1.equals("+")) {
+				int result = a+b;
+				System.out.println("a와b를 더한 값은? " + result);
+			} else if (s1.equals("-")) {
+				int result = a-b;
+				System.out.println("a와b를 뺀 값은? " + result);
+			} else if (s1.equals("*")) {
+				int result = a*b;
+				System.out.println("a와b를 곱한 값은? " + result);
+			} else if (s1.equals("/")) {
+				int result = a/b;
+				System.out.println("a와b를 나눈 값은? " + result);
+			} 
+
+		}
+*/
+		int a =1;
+		int b=2;
+		a=3;
+		b=5;
+		
+	}
+}
